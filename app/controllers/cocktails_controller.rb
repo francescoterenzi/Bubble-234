d435bf61640b19c817bf13b7218db72a65d3312f
valenico <valerio.nicolanti@hotmail.it>
2018-05-15 11:49:16 +0200

    cockatils controller create

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,26 +1,17 @@
 class CocktailsController < ApplicationController
-    #before_action :authenticate_user!, except: :show
-    before_action :has_user, :only => [:new, :create]
-    protected
-    def has_user
-        unless current_user
-            flash[:warning] = 'You must be logged in to create new cocktails.'
-            redirect_to new_user_session_path
-        end
-        unless (@user = User.find(:id => params[:user_id]))
-            flash[:warning] = 'Cocktails must be created from an existing user'
-            redirect_to root_path
-        end
-    end
+    before_action :authenticate_user!, except: :show
 
-    public
     def new
-     @cocktail = @user.cocktails.build
+        @cocktail = @user.cocktails.build
     end
 
     def create
-     current_user.cocktails << @user.cocktails.build(params[:cocktail])
-     redirect_to cocktail_path(@cocktail)
+        @cocktail = current_user.cocktails.build(cocktail_params)
+        if @cocktail.save
+            redirect_to cocktail_path(@cocktail)
+        else
+            render 'new'
+        end
     end
 
     def show
