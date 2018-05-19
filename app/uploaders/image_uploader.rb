075d0cc96c123940a77855b90a029ee9033a1077
valenico <valerio.nicolanti@hotmail.it>
2018-05-19 09:50:52 +0200

    resized cocktail images and updated show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,6 +30,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process resize_to_fit: [600, 600]
+
   # Create different versions of your uploaded files:
   version :thumb do
     process resize_to_fit: [50, 50]
