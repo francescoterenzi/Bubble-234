804b6f6eae1c2d091a89780249ad82a592abed45
marcomcl96 <ashba1996@gmail.com>
2018-05-29 19:59:20 +0200

    Conversations Graphics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,10 +29,10 @@ window.setTimeout(function() {
       var panel = $(this).parent().parent();
       var messages_list = panel.find('.messages-list');
   
-      panel.find('.panel-body').toggle();
-      panel.attr('class', 'panel panel-default');
+      panel.find('.card-body').toggle();
+      panel.attr('class', 'card border-dark');
   
-      if (panel.find('.panel-body').is(':visible')) {
+      if (panel.find('.card-body').is(':visible')) {
         var height = messages_list[0].scrollHeight;
         messages_list.scrollTop(height);
       }
--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -8,23 +8,22 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
       var conversation_visible = conversation.is(':visible');
 
       if (conversation_visible) {
-        var messages_visible = (conversation).find('.panel-body').is(':visible');
+        var messages_visible = (conversation).find('.card-body').is(':visible');
 
         if (!messages_visible) {
-          conversation.removeClass('panel-default').addClass('panel-success');
+          conversation.removeClass('border-dark').addClass('border-success');
         }
         conversation.find('.messages-list').find('ul').append(data['message']);
       }
       else {
         $('#conversations-list').append(data['window']);
         conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
-        conversation.find('.panel-body').toggle();
+        conversation.find('.card-body').toggle();
       }
     }
     else {
       conversation.find('ul').append(data['message']);
     }
-
     var messages_list = conversation.find('.messages-list');
     var height = messages_list[0].scrollHeight;
     messages_list.scrollTop(height);
